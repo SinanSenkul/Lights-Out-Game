06e27baa417df1e7b416de9fb46ee191e582f9d7
Sinan Şenkul <101463814+SinanSenkul@users.noreply.github.com>
2023-01-09 15:19:10 +0300

    Update Cell.js

--- src/Cell.js
+++ src/Cell.js
@@ -18,25 +18,3 @@ Cell.defaultProps = {
 }
 
 export default Cell
-
-/* import React, { Component } from "react";
-import "./Cell.css";
-
-class Cell extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(evt) {
-    this.props.flipCellsAroundMe();
-  }
-
-  render() {
-    let classes = "Cell" + (this.props.isLit ? " Cell-lit" : "");
-
-    return <td className={classes} onClick={this.handleClick} />;
-  }
-}
-
-export default Cell; */
